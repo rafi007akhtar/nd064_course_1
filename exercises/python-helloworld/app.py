fcf307727d5ac601df062cf74db19d3b6470a577
Md Rafi Akhtar <alimdrafi@gmail.com>
2021-07-04 01:34:04 +0530

    Completed Exercises for Lesson 2
    - added the endpoints status and metrics to the Flask application python-helloworld
    - returned 200 status along with the relevant JSON objects for each of those endpoints
    - added logging method to log the timestamp and metric when they are accessed

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,51 @@
+import logging
+from datetime import datetime
 from flask import Flask
+from werkzeug.wrappers import response
+from flask import json
 app = Flask(__name__)
+
+def get_info_log(endpoint):
+    log = f"{datetime.now()}, {endpoint} was reached successfully"
+    app.logger.info(log)
 
 @app.route("/")
 def hello():
+    get_info_log("root")
     return "Hello World!"
 
+@app.route("/status")
+def status():
+    response = app.response_class(
+        response = json.dumps({
+            "result": "OK - healthy"
+        }),
+        status = 200,
+        mimetype = "application/json"
+    )
+
+    get_info_log("status")
+    return response
+
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response = json.dumps({
+            "data": {
+                "UserCount": 140,
+                "UserCountActive": 23
+            }
+        }),
+        status = 200,
+        mimetype = "application/json"
+    )
+
+    get_info_log("metrics")
+    return response
+
 if __name__ == "__main__":
+    logging.basicConfig(
+        filename="app.log",
+        level=logging.DEBUG
+    )
     app.run(host='0.0.0.0')
